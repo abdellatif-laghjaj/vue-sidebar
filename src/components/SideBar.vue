5b9a7a7600544e3d1ffb8bbc8bc1e6221e1fce00
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-09-13 12:33:08 +0100

    save sidebar state

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -52,10 +52,11 @@
 <script setup>
 import { ref } from 'vue'
 
-const is_expanded = ref(false)
+const is_expanded = ref(localStorage.getItem("is_expanded") === "true")
 
 const toggleMenu = () => {
   is_expanded.value = !is_expanded.value
+  localStorage.setItem("is_expanded", is_expanded.value)
 }
 </script>
 
