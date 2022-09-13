9acf18761c41bef36a00869f6eec50dba66b3857
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-09-13 11:20:46 +0100

    style the sidebar

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,11 @@
 import SideBar from './components/SideBar.vue'
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 :root {
   --primary: #4ade80;
   --grey: #64748b;
-  --dark: D #1e293b;
+  --dark: #1e293b;
   --dark-alt: #334155;
   --light: #f1f5f9;
   --sidebar-width: 300px;
--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,11 +1,56 @@
 <template>
-  <aside>
-    SideBar
+  <aside :class="`${is_expanded ? 'is_expanded' : ''}`">
+    <!-- Logo -->
+    <div class="logo">
+      <img src="../assets/vue.svg" /> 
+    </div>
+
+    <!-- Menu -->
+    <div class="menu-toggle-wrap">
+      <button class="menu-toggle" @click="toggleMenu">
+        <box-icon name='right-arrow-square' type='solid' color='#fffefe' ></box-icon>
+      </button>
+    </div>
   </aside>
 </template>
+
 <script setup>
+  import { ref } from 'vue'
 
+  const is_expanded = ref(false)
+
+  const toggleMenu = () => {
+    is_expanded.value = !is_expanded.value
+  }
 </script>
-<style lang="scss">
 
+<style lang="scss">
+aside{
+  display: flex;
+  flex-direction: column;
+  background-color: var(--dark);
+  color: var(--light);
+  width: calc(2rem + 32px);
+  overflow: hidden;
+  min-height: 100vh;
+  padding: 1rem;
+  transition: 0.3s ease-out;
+
+  &.is_expanded{
+    width: var(--sidebar-width);
+  }
+
+  .logo{
+    margin-bottom: 1rem;
+
+    img{
+      width: 2rem;
+    }
+  }
+
+  @media (max-width: 768px){
+    position: fixed;
+    z-index: 99;
+  }
+}
 </style>
